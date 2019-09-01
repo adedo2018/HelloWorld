a332c7b41476c74517a21dea17642d1285726b81
adedo2018 <37647060+adedo2018@users.noreply.github.com>
2019-09-02 00:51:42 +0100

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -5,10 +5,10 @@ TOMCAT_DIR=/Users/axhol/Documents/apache-tomcat-9.0.11
 
 # Deploy the .war file to tomcat and restart the server
 cp $APP_NAME.jar $TOMCAT_DIR/webapps/$APP_NAME.jar
-$TOMCAT_DIR/bin/shutdown.sh
-sleep 1
+# $TOMCAT_DIR/bin/shutdown.sh
+# sleep 1
 $TOMCAT_DIR/bin/startup.sh
 sleep 1
 
 # Open the application in the default browser
-xdg-open http://localhost:8085/$APP_NAME
+# xdg-open http://localhost:8085/$APP_NAME
