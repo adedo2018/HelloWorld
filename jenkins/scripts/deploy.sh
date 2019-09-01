032441b05317b12c8ea4d8ff1fe2374b50ca2726
adedo2018 <37647060+adedo2018@users.noreply.github.com>
2019-09-02 00:46:47 +0100

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,7 +4,7 @@ APP_NAME=HelloWorld-0.1.0
 TOMCAT_DIR=/Users/axhol/Documents/apache-tomcat-9.0.11
 
 # Deploy the .war file to tomcat and restart the server
-mv $APP_NAME.jar $TOMCAT_DIR/webapps/$APP_NAME.jar
+cp $APP_NAME.jar $TOMCAT_DIR/webapps/$APP_NAME.jar
 $TOMCAT_DIR/bin/shutdown.sh
 sleep 1
 $TOMCAT_DIR/bin/startup.sh
