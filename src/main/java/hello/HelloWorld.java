28c34538a64452c7e281e0723dc76774db865708
adedo2018 <37647060+adedo2018@users.noreply.github.com>
2019-09-02 01:05:58 +0100

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,12 +1,19 @@
 package hello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ import org.springframework.boot.SpringApplication;
+ import org.springframework.boot.autoconfigure.SpringBootApplication;
+ import org.springframework.boot.builder.SpringApplicationBuilder;
+ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+ 
 @SpringBootApplication
-public class HelloWorld {
+public class HelloWorld extends SpringBootServletInitializer{
 
-    public static void main(String[] args) {
-        SpringApplication.run(HelloWorld.class, args);
-    }
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder app) { 
+  return app.sources(HelloWorld.class); 
 }
+
+public static void main(String[] args){
+SpringApplication.run(HelloWorld.class, args);
+ }
+ }
